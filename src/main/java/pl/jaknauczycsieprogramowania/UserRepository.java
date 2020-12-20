98b280e24bdd5c599958c985cf04543f396680d3
Łukasz Arndt <lukarn@gmail.com>
2020-12-21 00:03:12 +0100

    add user v01

---
+++ src/main/java/pl/jaknauczycsieprogramowania/UserRepository.java
@@ -0,0 +1,7 @@
+package pl.jaknauczycsieprogramowania;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ src/main/java/pl/jaknauczycsieprogramowania/UserService.java
@@ -0,0 +1,35 @@
+package pl.jaknauczycsieprogramowania;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @GetMapping("/users")
+    public ResponseEntity getUsers() throws JsonProcessingException {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(objectMapper.writeValueAsString(users));
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity addUser(User user) {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+
+}
